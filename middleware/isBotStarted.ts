12ede55a66923831bbdb77e91bb1ef461b017063
Michal Orlik <sogehige@gmail.com>
2021-04-15 16:43:25 +0200

    fix(startup): startup should wait until bot starts

--- middleware/isBotStarted.ts
+++ middleware/isBotStarted.ts
@@ -4,25 +4,27 @@ import { getTranslations,getConfiguration } from '@sogebot/ui-helpers/socket'
 import { populateListOf } from '@sogebot/ui-helpers/getListOf'
 import { setLocale } from '@sogebot/ui-helpers/dayjsHelper'
 
-export default async function ({ store }: { store: any }) {
-  isBotStarted(store)
-  store.commit('setLoggedUser', await isUserLoggedIn(false, false));
-  store.commit('setConfiguration', await getConfiguration());
+export default function ({ store }: { store: any }) {
+  (async function init () {
+    await isBotStarted(store)
+    store.commit('setLoggedUser', await isUserLoggedIn(false, false));
+    store.commit('setConfiguration', await getConfiguration());
 
-  await populateListOf('core');
-  await populateListOf('systems');
-  await populateListOf('integrations');
+    await populateListOf('core');
+    await populateListOf('systems');
+    await populateListOf('integrations');
 
-  /* we should commit to store
-  Vue.prototype.$core = await getListOf('core');
-  Vue.prototype.$systems = getListOf('systems');
-  Vue.prototype.$integrations = await getListOf('integrations');
-  */
+    /* we should commit to store
+    Vue.prototype.$core = await getListOf('core');
+    Vue.prototype.$systems = getListOf('systems');
+    Vue.prototype.$integrations = await getListOf('integrations');
+    */
 
-  await getTranslations();
-  const configuration = await getConfiguration();
-  store.commit('setConfiguration', configuration);
-  setLocale(configuration.lang as string);
+    await getTranslations();
+    const configuration = await getConfiguration();
+    store.commit('setConfiguration', configuration);
+    setLocale(configuration.lang as string);
 
-  store.commit('setUILoaded');
+    store.commit('setUILoaded');
+  })();
 }
